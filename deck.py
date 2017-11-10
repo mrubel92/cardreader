82a342e3617ebd05c0f5b931996ce1ff4fa16d43
Matt Rubel <mrubel92@gmail.com>
2017-11-10 15:04:56 -0500

    Initial commit
    Add initial deck structure

---
+++ deck.py
@@ -0,0 +1,60 @@
+from random import shuffle
+
+class Card:
+	def __init__(self, rank, suit):
+		self.rank = rank
+		self.suit = suit
+	def getRank(self):
+		return self.rank
+	def getSuit(self):
+		return self.suit
+	def getCardStr(self):
+		return self.rank + self.suit
+
+class Deck:
+	def __init__(self):
+		rank = "0"
+		suit = " "
+		count = 0
+		self.dealt = 0
+		self.deckArray = []
+		for r in range(1, 14):
+			if r == 1:
+				rank = "A"
+			elif r == 10:
+				rank = "T"
+			elif r == 11:
+				rank = "J"
+			elif r == 12:
+				rank = "Q"
+			elif r == 13:
+				rank = "K"
+			else:
+				rank = str(r)
+			for s in range(1, 5):
+				if s == 1:
+					suit = "s"
+				if s == 2:
+					suit = "c"
+				if s == 3:
+					suit = "d"
+				if s == 4:
+				    suit = "h"
+					
+				newCard = Card(rank, suit)
+				
+				self.deckArray.append(newCard)
+				count += 1
+				
+	def dispDeck(self):
+		'''This method for debugging purposes '''
+		for c in range(0, len(self.deckArray)):
+			print(self.deckArray[c].getRank() + self.deckArray[c].getSuit())
+	
+	def shuffleDeck(self):
+		shuffle(self.deckArray)
+		
+	def dealCard(self):
+		dealing = self.deckArray[self.dealt]
+		self.dealt += 1
+		return dealing
